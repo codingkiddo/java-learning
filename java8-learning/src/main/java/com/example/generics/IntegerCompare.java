cd7fd2fec2e60a43639db1ef5421f60387a73e08
codingkiddo <codingkiddo@gmail.com>
2024-02-13 17:49:47 +0530

    learning streams

---
+++ java8-learning/src/main/java/com/example/generics/IntegerCompare.java
@@ -0,0 +1,18 @@
+package com.example.generics;
+
+public class IntegerCompare {
+
+	public static void main(String[] rgs) {
+		
+		Integer i1 = 130;
+		Integer i2 = 130;
+		
+		System.out.println(i1 == i2);
+		
+		Integer n1 = 2000;
+		Integer n2 = 2000;
+		
+		System.out.println(n1 == n2);
+		
+	}
+}
---
+++ java8-learning/src/main/java/com/example/java8/features/StreamsMain.java
@@ -0,0 +1,11 @@
+package com.example.java8.features;
+
+public class StreamsMain {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
