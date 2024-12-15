8a7c5a1d55c3cb13f0a10150a6a4d06f0a876064
codingkiddo <codingkiddo@gmail.com>
2024-12-15 17:21:44 +0530

    Equals operator interview questions

---
+++ java8-learning/src/main/java/com/example/java8/features/EqualsOperator.java
@@ -0,0 +1,14 @@
+package com.example.java8.features;
+
+public class EqualsOperator {
+
+	public static void main(String[] args) {
+		Integer a = 128;
+		Integer b = 128;
+		System.out.println(a == b); // false
+
+		Integer x = 1;
+		Integer y = 1;
+		System.out.println(x == y); // true
+	}
+}
