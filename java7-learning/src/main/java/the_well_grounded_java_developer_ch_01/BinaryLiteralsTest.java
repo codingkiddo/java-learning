adaa8ef0bd9feed770d530a5f57481abbe457b09
codingkiddo <codingkiddo@gmail.com>
2024-12-19 19:37:46 +0530

    Baeldung tutorials going through

---
+++ java7-learning/src/main/java/the_well_grounded_java_developer_ch_01/BinaryLiteralsTest.java
@@ -0,0 +1,12 @@
+package the_well_grounded_java_developer_ch_01;
+
+public class BinaryLiteralsTest {
+
+	public static void main(String[] args) {
+
+		int x = Integer.parseInt("1100110", 2);
+		System.out.println(x);
+		
+	}
+
+}
