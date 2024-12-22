98c2684e66b66cfd0b3dfd5553f08fe7427c685b
codingkiddo <codingkiddo@gmail.com>
2024-12-22 18:54:48 +0530

    added java 8 certification problems

--- java8-learning/src/main/java/com/java8certification/Test_1.java
+++ java8-learning/src/main/java/com/java8certification/Test_1.java
@@ -12,7 +12,13 @@ public class Test_1 {
 	}
 	
 	public static void main(String[] args) {
+		Product product = new Product();
+		product.price = 200;
+		double newPrice = 100;
 		
+		Test_1 t = new Test_1();
+		t.updatePrice(product, newPrice);
+		System.out.println(product.price + " : " + newPrice);
 	}
 
 }
