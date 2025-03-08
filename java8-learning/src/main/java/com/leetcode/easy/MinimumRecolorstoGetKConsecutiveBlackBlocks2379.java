906a3b3d9112b2ed3f3ef7286544970de85f171c
codingkiddo <codingkiddo@gmail.com>
2025-03-08 08:13:23 +0530

    MinimumRecolorstoGetKConsecutiveBlackBlocks2379

---
+++ java8-learning/src/main/java/com/leetcode/easy/MinimumRecolorstoGetKConsecutiveBlackBlocks2379.java
@@ -0,0 +1,15 @@
+package com.leetcode.easy;
+
+public class MinimumRecolorstoGetKConsecutiveBlackBlocks2379 {
+	public int minimumRecolors(String blocks, int k) {
+        int blackCount = 0, ans = Integer.MAX_VALUE;
+    
+        for (int i = 0; i < blocks.length(); i++) {
+            if (i - k >= 0 && blocks.charAt(i - k) == 'B') blackCount--;
+            if (blocks.charAt(i) == 'B') blackCount++;
+            ans = Math.min(ans, k - blackCount);
+        }
+        
+        return ans;
+    }
+}
