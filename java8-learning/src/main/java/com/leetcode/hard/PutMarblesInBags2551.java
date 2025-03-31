5733a313ce49617822beb104edfa8c59158bcf75
codingkiddo <codingkiddo@gmail.com>
2025-03-31 07:28:54 +0530

    2551. Put Marbles in Bags

---
+++ java8-learning/src/main/java/com/leetcode/hard/PutMarblesInBags2551.java
@@ -0,0 +1,28 @@
+package com.leetcode.hard;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PutMarblesInBags2551 {
+	public long putMarbles(int[] weights, int k) {
+        if (k == 1) {
+            return 0;
+        }
+
+        List<Integer> pairSums = new ArrayList<>();
+        for (int i = 0; i < weights.length - 1; i++) {
+            pairSums.add(weights[i] + weights[i + 1]);
+        }
+
+        Collections.sort(pairSums);
+
+        long minScore = 0, maxScore = 0;
+        for (int i = 0; i < k - 1; i++) {
+            minScore += pairSums.get(i);
+            maxScore += pairSums.get(pairSums.size() - 1 - i);
+        }
+
+        return maxScore - minScore;
+    }
+}
