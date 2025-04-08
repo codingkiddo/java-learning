bf384fef3133d526f71dc9cb24a0d094872fd63f
codingkiddo <codingkiddo@gmail.com>
2025-04-08 18:22:39 +0530

    added class to get machine information

---
+++ java8-learning/src/main/java/com/example/class_loading/GetRAMInfo2.java
@@ -0,0 +1,27 @@
+package com.example.class_loading;
+
+
+
+import oshi.SystemInfo;
+import oshi.hardware.GlobalMemory;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.util.FormatUtil;
+
+public class GetRAMInfo2 {
+    public static void main(String... args) {
+    	
+    	
+        SystemInfo systemInfo = new SystemInfo();
+        HardwareAbstractionLayer hardware = systemInfo.getHardware();
+        GlobalMemory globalMemory = hardware.getMemory();
+        
+        long usedMemory = globalMemory.getTotal() - globalMemory.getAvailable();
+        
+        System.out.println(hardware.getProcessor());
+
+        System.out.println("Total memory: " + FormatUtil.formatBytes(globalMemory.getTotal()));
+        System.out.println("Available memory: " + FormatUtil.formatBytes(globalMemory.getAvailable()));
+        System.out.println("Used memory: " + FormatUtil.formatBytes(usedMemory));
+    }
+}
+
