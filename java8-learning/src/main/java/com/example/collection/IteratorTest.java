6deb8a08aa6847d1bd504f4cb203d445914a9ae2
codingkiddo <codingkiddo@gmail.com>
2025-04-10 09:11:23 +0530

    CountTheNumberOfPowerfulIntegers2999

--- java8-learning/src/main/java/com/example/collection/IteratorTest.java
+++ java8-learning/src/main/java/com/example/collection/IteratorTest.java
@@ -1,10 +1,29 @@
 package com.example.collection;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class IteratorTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		List<String> names = Arrays.asList("Vinod", "Kumar", "Coding", "Kiddo");
+
+		System.out.println("Before Java 5");
+		for ( Iterator<String> it = names.iterator(); it.hasNext(); ) {
+			String name = it.next();
+			System.out.println(name.length());
+		}
+		System.out.println("On/After Java 5");
+		for ( String name : names ) {
+			System.out.println(name.length());
+		}
+		
+		System.out.println("From Java 8 - 1");
+		names.forEach(name -> System.out.println(name.length()));
+		System.out.println("From Java 8 - 2");
+		names.forEach(System.out.println(String::length));
 	}
 
 }
