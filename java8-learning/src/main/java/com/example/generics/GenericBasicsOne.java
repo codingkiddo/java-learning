86867ba5b609c1dd0a1405e81fa599602391c033
codingkiddo <codingkiddo@gmail.com>
2025-04-13 14:03:24 +0530

    Modified streams class

--- java8-learning/src/main/java/com/example/generics/GenericBasicsOne.java
+++ java8-learning/src/main/java/com/example/generics/GenericBasicsOne.java
@@ -2,6 +2,7 @@ package com.example.generics;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GenericBasicsOne {
@@ -11,8 +12,6 @@ public class GenericBasicsOne {
 		List<Integer> numbers = Arrays.asList(1, 2, 3);
 		Stream<Integer> stream = numbers.stream();
 		
-		
-		
 		stream.onClose(new Runnable() {
 			@Override
 			public void run() {
@@ -20,6 +19,7 @@ public class GenericBasicsOne {
 			}
 		});
 		
+		System.out.println(stream.filter(n -> n/2==0).collect(Collectors.toList()));;
 		stream.close();
 	}
 }
