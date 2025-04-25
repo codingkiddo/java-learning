b32e08ce42272bd5f5242f4408e6f3525c343ebf
Vinod Kumar <vinodkumar@192.168.1.3>
2025-04-25 15:47:37 +0530

    CountOfInterestingSubarrays2845

---
+++ java8-learning/src/main/java/com/leetcode/medium/CountOfInterestingSubarrays2845.java
@@ -0,0 +1,24 @@
+package com.leetcode.medium;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CountOfInterestingSubarrays2845 {
+	public long countInterestingSubarrays(List<Integer> nums, int m, int k) {
+        long total = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int count = 0;
+
+        for (int num : nums) {
+            if (num % m == k) count++;
+            int remainder = (count - k) % m;
+            if (remainder < 0) remainder += m;
+            total += map.getOrDefault(remainder, 0);
+            map.put(count % m, map.getOrDefault(count % m, 0) + 1);
+        }
+
+        return total;
+    }
+}
