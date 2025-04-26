fc364b4fffdcba4997ec25b45019f788ddfc1c08
Vinod Kumar <vinodkumar@192.168.1.2>
2025-04-26 14:43:24 +0530

    CountSubarraysWithFixedBounds2444

---
+++ java8-learning/src/main/java/com/leetcode/hard/CountSubarraysWithFixedBounds2444.java
@@ -0,0 +1,18 @@
+package com.leetcode.hard;
+
+public class CountSubarraysWithFixedBounds2444 {
+	public long countSubarrays(int[] nums, int minK, int maxK) {
+        long count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+            for (int j = i; j < nums.length; j++) {
+                mini = Math.min(mini, nums[j]);
+                maxi = Math.max(maxi, nums[j]);
+                if (mini == minK && maxi == maxK) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
