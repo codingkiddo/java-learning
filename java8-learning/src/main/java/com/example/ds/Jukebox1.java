71bd5d8d6a64a4e11bb14ccf9a819dd1f47b34e0
codingkiddo <codingkiddo@gmail.com>
2025-05-05 07:36:01 +0530

    java8-learning/src/main/java/com/example/ds/Jukebox1.java 	java8-learning/src/main/java/com/leetcode/medium/DominoAndTrominoTiling790.java

---
+++ java8-learning/src/main/java/com/example/ds/Jukebox1.java
@@ -0,0 +1,36 @@
+package com.example.ds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jukebox1 {
+	public static void main(String[] args) {
+		new Jukebox1().go();
+	}
+
+	public void go() {
+		List<String> songList = MockSongs.getSongStrings();
+		System.out.println(songList);
+	}
+}
+
+class MockSongs {
+	public static List<String> getSongStrings() {
+//		In the real you'll often see Java Code that returns the interface type ( List ) and hides the implementation type ( ArrayList )
+		List<String> songs = new ArrayList<>();
+		songs.add("somersault");
+		songs.add("cassidy");
+		songs.add("$10");
+		songs.add("havana");
+		songs.add("Cassidy");
+		songs.add("50 Ways");
+		return songs;
+	}
+}
+
+
+/* OUTPUT:  
+
+[somersault, cassidy, $10, havana, Cassidy, 50 Ways]
+
+*/
