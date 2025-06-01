841368d27e9c343187ad91e02592c89c28d2aadf
Vinod Kumar <vinodkumar@192.168.1.4>
2025-06-01 19:39:49 +0530

    new file:   src/main/java/com/example/datetime/DaysToSeconds.java

---
+++ java8-learning/src/main/java/com/example/datetime/DaysToSeconds.java
@@ -0,0 +1,14 @@
+package com.example.datetime;
+
+public class DaysToSeconds {
+
+	public static void main(String[] args) {
+
+//		System.out.println(Long.parseLong("30*86400"));
+		System.out.println((30*86400));
+		
+		System.out.println((2592000/30));
+		
+	}
+
+}
