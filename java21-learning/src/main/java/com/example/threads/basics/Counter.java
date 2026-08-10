1f643df7924a7c2aef372998b958a7caed28602d
Vinod Kumar <codingkiddo@gmail.com>
2026-08-10 13:36:27 +0530

    Add Java concurrency basics examples

---
+++ java21-learning/src/main/java/com/example/threads/basics/Counter.java
@@ -0,0 +1,16 @@
+package com.example.threads.basics;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Counter {
+
+	private final AtomicInteger count = new AtomicInteger(0);
+	
+	public void increment() {
+		count.incrementAndGet();
+	}
+	
+	public int getCount() {
+		return count.get();
+	}
+}
